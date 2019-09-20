5042ddf5b259a9fb580804995116c184ef390b77
Ethan Collins <ethantcollins98@gmail.com>
2019-09-20 14:26:23 -0600

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
-  constructor() {
-    super()
 
-    this.state = {
+    state = {
       name: '',
       hp: '',
       frontUrl: '',
       backUrl: ''
     }
+
+  handleSubmit = () => {
+    this.props.search(this.state)
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
   }
 
   render() {
@@ -19,10 +24,10 @@ class PokemonForm extends React.Component {
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange} />
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" value={this.state.hp} onChange={this.handleChange} />
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" value={this.state.frontUrl} onChange={this.handleChange} />
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" value={this.state.backUrl} onChange={this.handleChange} />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
