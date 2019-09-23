0de997ef82b0581759d2452a7a87f5f684bc0d1f
Taylor <stein0209@gmail.com>
2019-09-23 12:49:13 -0600

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,26 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+      .then(response => response.json())
+      .then(pokemons => this.setState({pokemons: pokemons}))
+  }
+
+  renderPokemonCards = () => {
+    return this.state.pokemons.map(pokemon => {
+      return <PokemonCard key={pokemon.id} pokemon={pokemon} />
+    })
+  }
+
   render() {
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        {this.renderPokemonCards()}
       </Card.Group>
     )
   }
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -8,21 +8,39 @@ class PokemonForm extends React.Component {
     this.state = {
       name: '',
       hp: '',
-      frontUrl: '',
-      backUrl: ''
+      front: '',
+      back: ''
     }
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }).then(response => response.json())
+    .then(res => console.log(res))
   }
 
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} onChange={this.handleChange}>
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="front" />
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="back" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
