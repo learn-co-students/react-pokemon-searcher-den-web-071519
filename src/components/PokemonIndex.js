1acb5fa56eac56402819a074827cce92d039e191
lukemenard <lukemenard@gmail.com>
2019-09-23 16:02:31 -0600

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,58 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemonCollection: [],
+    searchTerm: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(response => response.json())
+    .then(results => this.setState({
+      pokemonCollection: results
+    }))
+  }
+
+  toggleImage = pokemon => {
+    const col = this.state.pokemonCollection
+    const i = col.indexOf(pokemon)
+    this.setState({
+      pokemonCollection: [
+        ...col.slice(0, i),
+        // initially pokemon.isClicked is undefined; inverting that falsey value makes it true
+        { ...pokemon, isClicked: !pokemon.isClicked },
+        ...col.slice(i + 1)
+      ]
+    })
+  }
+
+  addPokemon = pokemon => {
+    this.setState({ pokemonCollection: [...this.state.pokemonCollection, pokemon] })
+  }
+
+  handleChange = ( event, {value} ) => {
+    this.setState({
+      searchTerm: value
+    })
+  }
+
   render() {
+
+    const desiredPokemon = this.state.pokemonCollection.filter(p =>
+      p.name.includes(this.state.searchTerm)
+    )
+
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <PokemonForm addPokemon={this.addPokemon}/>
         <br />
-        <PokemonCollection />
+        <Search onSearchChange={_.debounce(this.handleChange, 500)} showNoResults={false} />
         <br />
-        <PokemonForm />
+        <PokemonCollection toggleImage={this.toggleImage} pokemon={desiredPokemon}/>
       </div>
     )
   }
